00f4dfdc377569f6f8f56d549af026d02e3fd2af
William Robersone <wmrobersone@yahoo.com>
2022-05-07 09:33:03 -0500

    add products

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            className="product-image"
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+          />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,33 @@ import React from 'react';
 import { FooterBanner, HeroBanner, Product } from '../components/index';
 import { client } from '../lib/client';
 
-const Home = () => {
-  return (
-    <div>
-      <HeroBanner />
-      <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
-      <div className="products-container">
-        {['Product 1', 'Product 2'].map((product) => product)}
-      </div>
-
-      <FooterBanner />
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+    <div className="products-heading">
+      <h2>Best Selling Products</h2>
+      <p>Speakers of many variations</p>
     </div>
-  );
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+
+    <FooterBanner />
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
