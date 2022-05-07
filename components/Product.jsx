27e66981ed0d718d16cbe18907688a8dab52ea50
William Robersone <wmrobersone@yahoo.com>
2022-05-07 15:05:37 -0500

    navbar and footer added

--- components/Product.jsx
+++ components/Product.jsx
@@ -13,6 +13,8 @@ const Product = ({ product: { image, name, slug, price } }) => {
             width={250}
             height={250}
           />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const Home = ({ products, bannerData }) => (
       ))}
     </div>
 
-    <FooterBanner />
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
   </div>
 );
 
